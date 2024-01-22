21f08132bda8f9d459de73fe40f761f74dc4dafa
v.podriez <tamtamlg@gmail.com>
2024-01-22 21:05:42 +0200

    sessions

--- catalog/views.py
+++ catalog/views.py
@@ -10,6 +10,9 @@ def index(request):
         status__exact='a').count()
     num_authors = Author.objects.count()
 
+    num_visits=request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     return render(
         request,
         'index.html',
@@ -17,7 +20,8 @@ def index(request):
             'num_books': num_books,
             'num_instances': num_instances,
             'num_instances_available': num_instances_available,
-            'num_authors': num_authors
+            'num_authors': num_authors,
+            'num_visits': num_visits,
         },
     )
 
